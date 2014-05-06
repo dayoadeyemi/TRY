1c6d14f6f1037064916effa3dd54285beb8234fd
unknown <DayoAdeyemi@HW021657.ad1.tm-gnet.com>
2014-05-06 17:40:39 +0100

    curried TRY

--- async-try.js
+++ async-try.js
@@ -2,7 +2,7 @@
  * TRY
  */
 var _ = require ('lodash');
-module.exports.TRY = _.curry(function (desc, fn, fail, succ){
+module.exports.TRY = function (desc, fn, fail, succ){
 	
 	function getErrorObject(){
 		try { throw Error('') } catch(err) { return err; }
@@ -11,24 +11,31 @@ module.exports.TRY = _.curry(function (desc, fn, fail, succ){
 	var err = getErrorObject();
 	var caller_line = err.stack.split("\n")[4];
 	var index = caller_line.indexOf("at ");
-	var clean = caller_line.slice(index+2, caller_line.length);
+	var line = caller_line.slice(index+2, caller_line.length);
 
   fn(function(){
     var argsArray = _.toArray(arguments);
     var error = _.first(argsArray);
-	var stackTrace
     var args = _.rest(argsArray);
     if (error) {
-      fail(error + "\n |"+ ((_.isString(desc) && !_.isEmpty(desc))?(" While " + desc):"") + " at:" + clean );
+      fail(error + "\n |"+ ((_.isString(desc) && !_.isEmpty(desc))?(" While " + desc):"") + " at:" + line );
     }
     else if (_.isFunction(succ)) {
       succ.apply(this, args);
     } else if (_.isFunction(fail)) {
       fail.apply(this, argsArray);
     }
-    
-    return function(printer){
-      printer(desc);
-    };
   });
-});
+  
+
+  
+  return function(printer){
+    printer(desc);
+  };
+};
+
+module.exports.TRY.to = function (desc, fn){
+	return function (callback){
+		module.exports.TRY(desc, fn, callback);
+	};
+};
